c24e80ac7f1a47b6b4aba1159f5dc02f5f0b6fb4
Joseph Wambele (TTC-Developer) <35718182+TTC-Developer@users.noreply.github.com>
2020-05-28 13:13:59 +0300

    index.php

---
+++ back_end/index.php
@@ -0,0 +1,9 @@
+<?php
+  class oopTestingClass {
+  
+  public $testing_oop_class = echo "Hello TTC Developers We Are Going To Use OOP For Our Project";
+  
+  }
+  $objectClass = new oopTestingClass;
+  echo $objectClass->testing_oop_class;
+?>
