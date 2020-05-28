672826518e186c1c7a7b0a1148e57ad4c57de45b
Joseph Wambele (TTC-Developer) <35718182+TTC-Developer@users.noreply.github.com>
2020-05-28 13:48:14 +0300

    Update index.php

--- back_end/index.php
+++ back_end/index.php
@@ -1,8 +1,19 @@
 <?php
   class oopTestingClass {
-  
-  public $testing_oop_class = echo "Hello TTC Developers We Are Going To Use OOP For Our Project";
-  
+  $username = "";
+    $password = "";
+    //...just a start
+  public $testing_oop_class = echo "Hello TTC Developers We Are Going To Use OOP For Our Project This Is Just An Example/Initial Commit Change It And Do Your Magic";
+    //method 1
+    public function login(){
+      // Wuuuh!! do the magic with $this username and password
+    }
+    //method 2
+    public function signup() {
+      // Wuuwh! do your magic with the inputs
+    }
+    
+   //......i see the future of this system in front of my eyes...
   }
   $objectClass = new oopTestingClass;
   echo $objectClass->testing_oop_class;
