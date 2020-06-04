431641741dd27c716d50fc29cc0e18b64f585a44
Joseph Wambele (TTC-Developer) <35718182+wambele63@users.noreply.github.com>
2020-06-04 19:56:55 +0300

    two new files added by @mchinja

---
+++ index.php
@@ -0,0 +1,170 @@
+<?php
+ include('control.php');
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<title>TOES LOGIN</title>
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+.input-container {
+  display: flex;
+  width: 100%;
+  margin-bottom: 15px;
+  border: 0px solid #dedede;
+  border-radius: 20px;
+  padding: 10px;
+  max-width: 1000px;
+  padding: 0 20px;
+  
+}
+.img {
+  border-radius: 50%;
+}
+
+.icon {
+  padding: 10px;
+  background: dodgerblue;
+  color: white;
+  min-width: 50px;
+  text-align: center;
+}
+
+.input-field {
+  width: 100%;
+  padding: 10px;
+  outline: none;
+  border-radius: 10px;
+}
+
+.input-field:focus {
+  border: 2px solid dodgerblue;
+  background: white;
+}
+
+/* Set a style for the submit button */
+.btn {
+  background-color: #0B5345;
+  color: white;
+  padding: 15px 20px;
+  border: none;
+  cursor: pointer;
+  width: 50%;
+  border-radius: 20px;
+  opacity: 0.9;
+}
+
+.btn:hover {
+  opacity: 1;
+}
+
+.edit{
+	color: white;
+}
+
+#header table tr td fieldset legend strong {
+    font-family: Gotham, Helvetica Neue, Helvetica, Arial, sans-serif;
+}
+#demo{
+  text-align: center;
+  font-size: 30px;
+  color: red;
+  margin-top: 0px;
+}
+
+fieldset{
+  color: white; 
+  background-image: url(uploads/filehome.jpg);
+  background-position: right bottom, left top;
+  background-repeat: no-repeat, repeat;
+  border-radius: 10px;
+ background-size: cover;
+}
+
+
+}
+</style>
+<center><img src="uploads/ttc logo.jpg" width="140" height="140" class="img" alt="logo"/></center>
+	
+</head>
+<body>
+    <center><strong><font face="verdana" color="#0C9BF1" size="5.9px">TANZANIA ONLINE EDUCATION SYSTEM [TOES]</font></strong> </center>
+   
+   <p id="demo"></p>
+<!--
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Apr 15, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "days " + hours + "hrs "
+  + minutes + "min " + seconds + "sec "+ "<br> left to deadline!!!";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "APPLICATION DEADLINE IS REACHED";
+  }
+}, 1000);
+</script>
+-->
+   <marquee scrolldelay="8" behavior="alternate" direction="left" ><h2 align="center"><strong>	
+	If you don't have an account<a href="signup.php" style="color:green "> REGISTER HERE</a>
+	</strong></h2></marquee>
+
+<form  action="" style="max-width:600px;margin:auto" method="POST" enctype="multipart/form-data">
+<fieldset>
+<strong><legend>LOGIN</legend></strong>
+<font color="red" align="center"><h6><b><?php echo display_error();?> </h6></b></font>
+
+<label for="textfield"><b>USER NAME:</b></label>
+<div class="input-container">
+    <i class="fa fa-user-circle icon" style='font-size:30px;color:white'></i>
+    <input class="input-field" type="text" placeholder="Write your firstname /email" name="username">
+  </div>	
+  
+<label for="password"><b>PASSWORD:</b></label>	
+<div class="input-container">
+    <i class="fa fa-key icon" style='font-size:30px;color:white' ></i>
+    <input class="input-field" type="password" placeholder="Write Your password" name="password">
+ </div>							
+<br>
+<div class="clearfix" align="center">
+<button type="submit"   name="login" class="btn">
+<b>LOGIN</b></button>
+</div>
+</fieldset>					
+<br>
+
+<h3><strong>If you forgot password <a href="reset_password.php" style="color:dodgerblue bold">RESET HERE</a></strong><Br><strong>	
+If you do not have account <a href="signup.php" style="color:green "> REGISTER HERE</a></strong></H3>
+			</form>
+
+  <p align="center"> Copyright &copy;. All rights reserved. </p>
+	        
+</body>
+</html>
